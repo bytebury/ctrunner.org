cc627e9920ad0e9fcd18b6c0ed75d7c7975d57ea
Marcello <marchsabino@gmail.com>
2025-12-03 18:55:35 -0500

    feat: add like and bought ticket icons to upcoming races

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -15,3 +15,10 @@ pub fn date<T: Display>(s: T, _: &dyn askama::Values) -> askama::Result<String> 
         Err(_) => Ok("Unknown Date".to_string()),
     }
 }
+
+pub fn daydatetime<T: Display>(s: T, _: &dyn askama::Values) -> askama::Result<String> {
+    match NaiveDateTime::parse_from_str(&s.to_string(), "%Y-%m-%d %H:%M:%S") {
+        Ok(dt) => Ok(dt.format("%a, %b %d, %Y at %-I:%M %p").to_string()),
+        Err(_) => Ok("Unknown Date and Time".to_string()),
+    }
+}
